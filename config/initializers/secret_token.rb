5ad3a8876d6bcf1ae32925ec72005ea9602a5408
SergeyBerdnikovich <atom@tut.by>
2013-06-18 19:25:40 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luxrent::Application.config.secret_token = '031c5a4ad6b64fd04e1e14e6d16d4de45d9546376c7cf5538578ab180ccbf60106c283505fc18e2b7baf6a1e8a191dce49ebbf8a6cafc31faebf466c78f2c0a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Luxrent::Application.config.session_store :cookie_store, key: '_luxrent_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Luxrent::Application.config.session_store :active_record_store
